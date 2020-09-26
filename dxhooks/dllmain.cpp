9f6e22df124d0bb2560e1af92137413e97ebc14f
KodyJKing <kody.j.king@gmail.com>
2020-09-25 17:07:18 -0700

    now hooking SetTexture method and unhooking when unloading

--- dxhooks/dllmain.cpp
+++ dxhooks/dllmain.cpp
@@ -1,31 +1,12 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include <Windows.h>
-#include <iostream>
-#include <string>
 #include "dllmain.h"
 
+#pragma comment(lib, "d3d9.lib")
+
+using namespace std;
+
 HMODULE myHModule;
-
-DWORD __stdcall myThread(LPVOID lpParameter) {
-    AllocConsole();
-    FILE* pFile;
-    auto err = freopen_s(&pFile, "CONOUT$", "w", stdout);
-
-    std::cout << "Hello from module:  " << std::to_string((int)myHModule) << std::endl;
-
-    int updateMeAndRecompileToTestUnloading = 42;
-    if (!err) {
-        while (TRUE) {
-            Sleep(100);
-            if (GetAsyncKeyState(VK_F9))
-                break;
-        }
-    }
-
-    if (pFile) fclose(pFile);
-    FreeConsole();
-    FreeLibraryAndExitThread(myHModule, 0);
-}
+HookRecord setTextureHookRecord;
 
 BOOL APIENTRY DllMain(HMODULE hModule,
     DWORD  ul_reason_for_call,
@@ -43,3 +24,68 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     return TRUE;
 }
 
+DWORD __stdcall myThread(LPVOID lpParameter) {
+    AllocConsole();
+    FILE* pFile;
+    auto err = freopen_s(&pFile, "CONOUT$", "w", stdout);
+
+    cout << "Hello from module:  " << to_string((int)myHModule) << endl;
+
+    hookSetTexture();
+
+    int updateMeAndRecompileToTestUnloading = 42;
+    if (!err) {
+        while (TRUE) {
+            Sleep(100);
+            if (GetAsyncKeyState(VK_F9))
+                break;
+        }
+    }
+
+    removeHook(setTextureHookRecord);
+    if (pFile) fclose(pFile);
+    FreeConsole();
+    FreeLibraryAndExitThread(myHModule, 0);
+}
+
+void** getDeviceVirtualTable() {
+    // Found device location by backtracing a device method call.
+    // Found device method using virtual table from dummy device.
+    // https://gist.github.com/KodyJKing/d7b374b29998dfdd7d631430164f3e50
+    IDirect3DDevice9** ppDevice = (IDirect3DDevice9**)0x0071D174;
+    IDirect3DDevice9* pDevice = *ppDevice;
+    void** vTable = *(void***)(pDevice);
+    return vTable;
+}
+
+HookRecord addHook(const char* description, void** vtable, int methodIndex, void* newMethod) {
+    cout << "Adding hook: " << description << endl;
+    HookRecord record{};
+    record.description = description;
+    record.vtable = vtable;
+    record.methodIndex = methodIndex;
+    record.newMethod = newMethod;
+    record.oldMethod = vtable[methodIndex];
+    vtable[methodIndex] = newMethod;
+    return record;
+}
+
+void removeHook(HookRecord record) {
+    cout << "Removing hook: " << record.description << endl;
+    record.vtable[record.methodIndex] = record.oldMethod;
+}
+
+void hookSetTexture() {
+    void** vTable = getDeviceVirtualTable();
+    setTextureHookRecord = addHook("SetTexture", vTable, 65, setTextureHook);
+}
+
+int callCounter = 0;
+HRESULT __stdcall setTextureHook(
+    IDirect3DDevice9* pThisDevice,
+    DWORD stage,
+    IDirect3DBaseTexture9* pTexture
+) {
+    if (callCounter++ % 100 == 0) cout << "logging from within hook!" << endl;
+    return ((SetTextureFunc)setTextureHookRecord.oldMethod)(pThisDevice, stage, pTexture);
+}
--- dxhooks/dllmain.h
+++ dxhooks/dllmain.h
@@ -1,3 +1,27 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <d3d9.h>
+
 #pragma once
 
+typedef HRESULT(__stdcall* SetTextureFunc)(IDirect3DDevice9* pThisDevice, DWORD stage, IDirect3DBaseTexture9* pTexture);
+
 DWORD __stdcall myThread(LPVOID lpParameter);
+
+void hookSetTexture();
+
+HRESULT __stdcall setTextureHook(IDirect3DDevice9* pThisDevice, DWORD stage, IDirect3DBaseTexture9* pTexture);
+
+void** getDeviceVirtualTable();
+
+struct HookRecord {
+    const char* description;
+    void** vtable;
+    int methodIndex;
+    void* oldMethod;
+    void* newMethod;
+};
+
+HookRecord addHook(const char* description, void** vtable, int methodIndex, void* newMethod);
+void removeHook(HookRecord record);
